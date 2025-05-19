ca83bc81d797f9ea6afde88e810f5bceb21c3a92
Hiennee <chanhien55@gmail.com>
2025-05-19 17:33:45 +0700

    Add project files.

---
+++ Models/MerchantAccountsZalopay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaloPayCallbackAPI.Models
+{
+    public partial class MerchantAccountsZalopay
+    {
+        public int UserId { get; set; }
+        public string CdShop { get; set; } = null!;
+        public string AppId { get; set; } = null!;
+        public string Key1 { get; set; } = null!;
+        public string Key2 { get; set; } = null!;
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ Models/ZaloPayCallbackDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaloPayCallbackAPI.Models
+{
+    public partial class ZaloPayCallbackDetail
+    {
+        public int AppId { get; set; }
+        public string AppTransId { get; set; } = null!;
+        public long AppTime { get; set; }
+        public string AppUser { get; set; } = null!;
+        public long Amount { get; set; }
+        public string EmbedData { get; set; } = null!;
+        public string Item { get; set; } = null!;
+        public string ZpTransId { get; set; } = null!;
+        public long ServerTime { get; set; }
+        public int Channel { get; set; }
+        public string MerchantUserId { get; set; } = null!;
+        public long UserFeeAmount { get; set; }
+        public long DiscountAmount { get; set; }
+    }
+}
