4ffd3136b313f56bb3045fb3bb2bee0299e308ab
Hiennee <chanhien55@gmail.com>
2025-05-26 13:50:29 +0700

    Add project files.

---
+++ Controllers/ZaloPayCallbackController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Newtonsoft.Json.Linq;
+using ZaloPayCallbackAPI.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using ZaloPay.Helper.Crypto;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZaloPayCallbackAPI.Controllers
+{
+    [ApiController]
+    [Route("api/zalo")]
+    public class ZaloPayCallbackController : ControllerBase
+    {
+        private readonly UnionPOSContext _context;
+
+        public ZaloPayCallbackController(UnionPOSContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("callback")]
+        public async Task<IActionResult> Post([FromBody] dynamic req)
+        {
+            JsonElement jsonElement = (JsonElement)req;
+            string jsonString = jsonElement.GetRawText();
+            JObject reqJson = JObject.Parse(jsonString);
+
+            var result = new Dictionary<string, object>();
+            string key2 = _context.MerchantAccountsZalopays.Select(zalo => zalo.Key2).FirstOrDefault() ?? "";
+            try
+            {
+                var dataStr = Convert.ToString(reqJson["data"]);
+                var reqMac = Convert.ToString(reqJson["mac"]);
+
+                var mac = HmacHelper.Compute(ZaloPayHMAC.HMACSHA256, key2, dataStr);
+
+                Console.WriteLine("mac = {0}", mac);
+
+                if (!reqMac.Equals(mac))
+                {
+                    result["return_code"] = -1;
+                    result["return_message"] = "mac not equal";
+                }
+                else
+                {
+                    var dataJson = JsonConvert.DeserializeObject<Dictionary<string, object>>(dataStr);
+                    Console.WriteLine("update order's status = success where app_trans_id = {0}", dataJson["app_trans_id"]);
+
+                    ZaloPayCallbackDetail data = new ZaloPayCallbackDetail();
+                    data.AppId = Convert.ToInt32(dataJson["app_id"]);
+                    data.AppTransId = Convert.ToString(dataJson["app_trans_id"]) ?? "";
+                    data.AppTime = Convert.ToInt64(dataJson["app_time"]);
+                    data.AppUser = Convert.ToString(dataJson["app_user"]) ?? "";
+                    data.Amount = Convert.ToInt64(dataJson["amount"]);
+                    data.EmbedData = Convert.ToString(dataJson["embed_data"]) ?? "";
+                    data.Item = Convert.ToString(dataJson["item"]) ?? "";
+                    data.ZpTransId = Convert.ToString(dataJson["zp_trans_id"]) ?? "";
+                    data.ServerTime = Convert.ToInt64(dataJson["server_time"]);
+                    data.Channel = Convert.ToInt32(dataJson["channel"]);
+                    data.MerchantUserId = Convert.ToString(dataJson["merchant_user_id"]) ?? "";
+                    data.UserFeeAmount = Convert.ToInt64(dataJson["user_fee_amount"]);
+                    data.DiscountAmount = Convert.ToInt64(dataJson["discount_amount"]);
+
+                    await _context.ZaloPayCallbackDetails.AddAsync(data);
+                    await _context.SaveChangesAsync();
+
+                    result["return_code"] = 1;
+                    result["return_message"] = "success";
+                }
+            }
+            catch (Exception ex)
+            {
+                result["return_code"] = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
+                result["return_message"] = ex.Message;
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ Models/UnionPOSContext.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ZaloPayCallbackAPI.Models
+{
+    public partial class UnionPOSContext : DbContext
+    {
+        public UnionPOSContext()
+        {
+        }
+
+        public UnionPOSContext(DbContextOptions<UnionPOSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<MerchantAccountsZalopay> MerchantAccountsZalopays { get; set; } = null!;
+        public virtual DbSet<ZaloPayCallbackDetail> ZaloPayCallbackDetails { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Korean_Wansung_CI_AS");
+
+            modelBuilder.Entity<MerchantAccountsZalopay>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.ToTable("MERCHANT_ACCOUNTS_ZALOPAY");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.Property(e => e.AppId)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("APP_ID");
+
+                entity.Property(e => e.CdShop)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("CD_SHOP");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Key1)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("KEY1");
+
+                entity.Property(e => e.Key2)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("KEY2");
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<ZaloPayCallbackDetail>(entity =>
+            {
+                entity.HasKey(e => e.AppTransId);
+
+                entity.ToTable("ZaloPay_Callback_Detail");
+
+                entity.Property(e => e.AppTransId)
+                    .HasMaxLength(256)
+                    .IsUnicode(false)
+                    .HasColumnName("APP_TRANS_ID");
+
+                entity.Property(e => e.Amount).HasColumnName("AMOUNT");
+
+                entity.Property(e => e.AppId).HasColumnName("APP_ID");
+
+                entity.Property(e => e.AppTime).HasColumnName("APP_TIME");
+
+                entity.Property(e => e.AppUser)
+                    .IsUnicode(false)
+                    .HasColumnName("APP_USER");
+
+                entity.Property(e => e.Channel).HasColumnName("CHANNEL");
+
+                entity.Property(e => e.DiscountAmount).HasColumnName("DISCOUNT_AMOUNT");
+
+                entity.Property(e => e.EmbedData).HasColumnName("EMBED_DATA");
+
+                entity.Property(e => e.Item).HasColumnName("ITEM");
+
+                entity.Property(e => e.MerchantUserId)
+                    .IsUnicode(false)
+                    .HasColumnName("MERCHANT_USER_ID");
+
+                entity.Property(e => e.ServerTime).HasColumnName("SERVER_TIME");
+
+                entity.Property(e => e.UserFeeAmount).HasColumnName("USER_FEE_AMOUNT");
+
+                entity.Property(e => e.ZpTransId)
+                    .IsUnicode(false)
+                    .HasColumnName("ZP_TRANS_ID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
